86722fbfb178e6f071f9c14763ddc60a86958ec2
Debora Souza Barros <98713839+Debby-Barros@users.noreply.github.com>
2023-01-21 20:48:21 -0300

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -3,90 +3,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct grafo
-{
-    int numero_vertices;
-    int grau_max;  // maximo de arestas que vao ser definidas no input
-    int **arestas; // matriz: linha=vertice coluna=arestas
-    int *grau;     // verifica quais ja foram inseridos e as prox posicoes vagas
-} Grafo;
+typedef struct no_lista {
+  int destino;
+  struct no_lista* prox;
+}no_lista;
 
-Grafo *cria_Grafo(int numero_vertices, int grau_max)
-{
-    Grafo *gr = (Grafo *)malloc(sizeof(struct grafo));
 
-    gr->numero_vertices = numero_vertices;
-    gr->grau_max = grau_max;
-    gr->grau = (int *)calloc(numero_vertices, sizeof(int)); // inicia com grau 0
+typedef struct grafo {
+  int tamanho;
+  int* visitado;
+  // armazenar uma matriz de lista de adjacencia
+  struct no_lista** lista_adj;
+}grafo;
 
-    gr->arestas = (int **)malloc(numero_vertices * sizeof(int *));
 
-    for (int i = 0; i < numero_vertices; i++)
-        // cada posicao possui uma lista de adjacencia que tem um tamanho de grau max
-        gr->arestas[i] = (int *)malloc(grau_max * sizeof(int));
-
-    return gr;
+// função para criar novo no
+no_lista* novoNo(int destino) {
+  no_lista* novoNo = (no_lista*) malloc(sizeof(no_lista));
+  novoNo->destino = destino;
+  novoNo->prox = NULL;
+  return novoNo;
 }
 
-int inserirArestas(Grafo *gr, int ini, int fim)
-{
-    if (gr == NULL)
-        return 0;
-    // verifica se o vertice eh valido
-    if (ini < 0 || ini >= gr->numero_vertices)
-        return 0;
-    if (fim < 0 || fim >= gr->numero_vertices)
-        return 0;
 
-    // insere no fim da linha
-    gr->arestas[ini][gr->grau[ini]] = fim;
-    gr->grau[ini]++;
+// Função para criar grafo
+grafo* criarGrafo(int tamanho) {
+  grafo* Grafo = (grafo*) malloc(sizeof(grafo));
+  Grafo->tamanho = tamanho;
+  Grafo->lista_adj = malloc(tamanho * sizeof(no_lista*));
+  Grafo->visitado = malloc(tamanho * sizeof(int));
 
-    return 1;
+  for (int i = 0; i < tamanho; i++) {
+    Grafo->lista_adj[i] = NULL;
+    Grafo->visitado[i] = 0;
+  }
+  return Grafo;
 }
 
-// funcao que auxilia a dfs realizando o calculo da busca
-void buscaProfundidade(Grafo *gr, int ini, int *visitado, int cont)
-{
-    // marca o vertice como visitado
-    visitado[ini] = cont;
-    // Visita os vizinhos desse vetice
-    for (int i = 0; i < gr->grau[ini]; i++)
-    {
-        // se nao foi visitado, executa o dfs e marca os vizinhos
-        if (!visitado[gr->arestas[ini][i]])
-            buscaProfundidade(gr, gr->arestas[ini][i], visitado, cont + 1);
+
+// Função para adicionar arestas na lista 
+void adicionarArestas(grafo* Grafo, int ponto, int destino) {
+
+  no_lista* novo_no = novoNo(destino);
+  novo_no->prox = Grafo->lista_adj[ponto];
+  Grafo->lista_adj[ponto] = novo_no;
+
+  // adicionar arestas do destino ate o ponto
+  novo_no = novoNo(ponto);
+  novo_no->prox = Grafo->lista_adj[destino];
+  Grafo->lista_adj[destino] = novo_no;
+}
+
+
+// Função do DFS
+void DFS(grafo* Grafo, int destino) {
+  no_lista* list_adjacencia = Grafo->lista_adj[destino];
+  no_lista* temp = list_adjacencia;
+  
+  printf("Visitados");
+
+  Grafo->visitado[destino] = 1;
+  printf("-> %d \n", destino);
+
+  while (temp != NULL) {
+    int conectaVertices = temp->destino;
+    if (Grafo->visitado[conectaVertices] == 0) {
+      DFS(Grafo, conectaVertices);
     }
+    temp = temp->prox;
+  }
 }
 
-// funcao principal da dfs
-void buscaProfundidade_Grafo(Grafo *gr, int ini, int *visitado)
-{
-    int cont = 1; // contar ordem de visitcao
-    // marca vertices como NAO visitados
-    for (int i = 0; i < gr->numero_vertices; i++)
-        visitado[i] = 0; // inicializa o vetor
-    buscaProfundidade(gr, ini, visitado, cont);
 
-    for (int i = 0; i < gr->numero_vertices; i++)
-        printf("%d -> %d\n", i, visitado[i]);
-}
+int main() {
+  grafo* Grafo = criarGrafo(4);
+  
+  adicionarArestas(Grafo, 0, 1);
+  adicionarArestas(Grafo, 0, 2);
+  adicionarArestas(Grafo, 1, 2);
+  adicionarArestas(Grafo, 2, 3);
 
-int main()
-{
+  DFS(Grafo, 2);
 
-    Grafo *gr = cria_Grafo(5, 5);
-
-    inserirArestas(gr, 0, 1);
-    inserirArestas(gr, 1, 3);
-    inserirArestas(gr, 1, 2);
-    inserirArestas(gr, 2, 4);
-    inserirArestas(gr, 3, 0);
-    inserirArestas(gr, 3, 4);
-    inserirArestas(gr, 4, 1);
-
-    int vis[5]; // vetor para marcar a ordem que os vertices foram visitados
-    buscaProfundidade_Grafo(gr, 0, vis);
-
-    return 0;
+  return 0;
 }
